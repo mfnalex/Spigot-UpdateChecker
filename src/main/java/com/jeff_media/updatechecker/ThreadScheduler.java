5dac8ad4939f5589b287f0568a2a879649450a5f
Paulem <66211574+Paulem79@users.noreply.github.com>
2024-08-30 17:12:42 +0200

    Update ThreadScheduler.java

--- src/main/java/com/jeff_media/updatechecker/ThreadScheduler.java
+++ src/main/java/com/jeff_media/updatechecker/ThreadScheduler.java
@@ -5,14 +5,14 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class ThreadScheduler {
+    // Create a ScheduledExecutorService with a single thread
+    public static ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
     public static ScheduledExecutorService runTask(Runnable task) {
         return runTask(task, 1/20L, TimeUnit.MILLISECONDS);
     }
 
     public static ScheduledExecutorService runTask(Runnable task, long delay, TimeUnit timeUnit) {
-        // Create a ScheduledExecutorService with a single thread
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-
         scheduler.schedule(task, delay, timeUnit);
 
         return scheduler;
